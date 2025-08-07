2c7c9ed8e733899d197b391ddec22094df7dfb0c
Cristian <duhhmendez@gmail.com>
2025-08-06 21:21:45 -0400

    Simplify modal exit to X button only

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -139,17 +139,6 @@ const Login = ({ onClose }) => {
                   {isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}
                 </button>
               </div>
-              
-              {/* Back to Home Option */}
-              <div className="text-center pt-4 border-t border-gray-200">
-                <button
-                  type="button"
-                  onClick={onClose}
-                  className="text-gray-500 hover:text-gray-700 text-sm"
-                >
-                  ← Back to Home
-                </button>
-              </div>
             </form>
           </CardContent>
         </Card>
